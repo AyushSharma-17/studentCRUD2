83249092566c3f39a9df4d8846fca552ed4ee568
ayush <ayushsharma12345qwerty@gmail.com>
2026-05-11 13:29:28 +0530

    error of lomok getter setter resolved

--- src/main/java/com/ayush/studentCRUD/Dao/student.java
+++ src/main/java/com/ayush/studentCRUD/Dao/student.java
@@ -5,17 +5,41 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Data
-@Getter
-@Setter
+
 public class student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private int id;
     private String name;
     private String email;
+
+
+    // --- Getters ---
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // --- Setters ---
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
